a2b0690f94461c0f5334d2b327e6be5051c14c7b
Arvind1709 <arvindyadav1709@gmail.com>
2025-10-11 13:36:08 +0530

    Create BookModel.cs
    BookModel is created

---
+++ BookLibraryMVC/Models/BookModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookLibraryMVC.Models
+{
+        public class BookModel
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public decimal Price { get; set; }
+            public int PublishedYear { get; set; }
+        }
+}
+
